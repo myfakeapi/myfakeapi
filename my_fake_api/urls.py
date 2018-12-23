c65a5d1ae45eccdc867696595288a83c3b4113d9
Aivis Abele <aabele@gmail.com>
2018-12-24 00:30:39 +0200

    * Added api authtoken; * Added unittests for `my_fake_api.api.views.APIViewSet`;

--- my_fake_api/urls.py
+++ my_fake_api/urls.py
@@ -3,6 +3,7 @@ Application url configuration
 """
 
 from django.urls import path, include
+from rest_framework.authtoken import views
 from my_fake_api import handlers
 
 from my_fake_api.api.routers import router
@@ -12,6 +13,7 @@ app_name = "my_fake_api"
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', views.obtain_auth_token),
 
     path("<uuid:api_id>/<path:path>", handlers.http_request_handler, name="http_handler"),
 ]
